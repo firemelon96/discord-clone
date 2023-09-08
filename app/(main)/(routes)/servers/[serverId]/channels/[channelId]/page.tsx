768207b2fae705f960f84120bb4a7c2cecbfde54
estong jamion <estong.jamion@gmail.com>
2023-09-08 10:08:21 +0300

    added upload component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,59 @@
-const ChannelPage = () => {
-  return <div>Channel Id page</div>;
+import { ChatHeader } from '@/components/chat/chat-header';
+import { ChatInput } from '@/components/chat/chat-input';
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+
+import { redirectToSignIn } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+interface ChannelPageProps {
+  params: {
+    channelId: string;
+    serverId: string;
+  };
+}
+
+const ChannelPage = async ({ params }: ChannelPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) return redirectToSignIn();
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    return redirect('/');
+  }
+
+  return (
+    <div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
+      <ChatHeader
+        serverId={channel.serverId}
+        name={channel.name}
+        type='channel'
+      />
+      <div className='flex-1'>Future Messages</div>
+      <ChatInput
+        apiUrl='/api/socket/messages'
+        name={channel.name}
+        type='channel'
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+      />
+    </div>
+  );
 };
 
 export default ChannelPage;
