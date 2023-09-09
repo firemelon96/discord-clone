c5890d7d51860b6836fa87f9df4ba036c80da010
estong jamion <estong.jamion@gmail.com>
2023-09-09 23:27:05 +0300

    Add chat item and edit chat by moderator and admin

--- pages/api/socket/messages/index.ts
+++ pages/api/socket/messages/index.ts
@@ -14,7 +14,7 @@ export default async function handler(
   try {
     const profile = await currentProfilePages(req);
 
-    const { content, fileUrl } = req.body();
+    const { content, fileUrl } = req.body;
     const { serverId, channelId } = req.query;
 
     if (!profile) return res.status(401).json({ error: 'Unauthorized' });
