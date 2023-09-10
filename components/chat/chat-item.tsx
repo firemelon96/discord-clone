242d138c852dcd75909ebe9e9790a175364f8fa8
estong jamion <estong.jamion@gmail.com>
2023-09-10 13:28:26 +0300

    modified the socket to work

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -16,6 +16,8 @@ import { UserAvatar } from '@/components/user-avatar';
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import { useModal } from '@/hooks/use-modal-store';
+import { useParams, useRouter } from 'next/navigation';
 
 interface ChatItemProps {
   id: string;
@@ -55,7 +57,17 @@ export const ChatItem = ({
   socketQuery,
 }: ChatItemProps) => {
   const [isEditing, setIsEditing] = useState(false);
-  const [isDeleting, setIsDeleting] = useState(false);
+  const { onOpen } = useModal();
+  const router = useRouter();
+  const params = useParams();
+
+  const onMemberClick = () => {
+    if (member.id === currentMember.id) {
+      return;
+    }
+
+    router.push(`/servers/${params?.serverId}/converssations/${member.id}`);
+  };
 
   useEffect(() => {
     const handleKeyDown = (event: any) => {
@@ -113,13 +125,19 @@ export const ChatItem = ({
   return (
     <div className='relative group flex items-center hover:bg-black/5 p-4 transition w-full'>
       <div className='group flex gap-x-2 items-start w-full'>
-        <div className='cursor-pointer hover:drop-shadow-md transition'>
+        <div
+          onClick={onMemberClick}
+          className='cursor-pointer hover:drop-shadow-md transition'
+        >
           <UserAvatar src={member.profile.imageUrl} />
         </div>
         <div className='flex flex-col w-full'>
           <div className='flex items-center gap-x-2'>
             <div className='flex items-center'>
-              <p className='font-semibold text-sm hover:underline cursor-pointer'>
+              <p
+                onClick={onMemberClick}
+                className='font-semibold text-sm hover:underline cursor-pointer'
+              >
                 {member.profile.name}
               </p>
               <ActionTooltip label={member.role}>
@@ -219,7 +237,15 @@ export const ChatItem = ({
             </ActionTooltip>
           )}
           <ActionTooltip label='Edit'>
-            <Trash className='cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition' />
+            <Trash
+              onClick={() =>
+                onOpen('deleteMessage', {
+                  apiUrl: `${socketUrl}/${id}`,
+                  query: socketQuery,
+                })
+              }
+              className='cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition'
+            />
           </ActionTooltip>
         </div>
       )}
